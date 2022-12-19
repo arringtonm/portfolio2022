5a047356ac7018522af7c396c920425abdb38662
Michael Arrington <31665551+arringtonm@users.noreply.github.com>
2022-12-18 21:17:47 -0800

    Add https redirect

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+
+if (window.location.protocol == 'http:') {
+  console.log(
+    'you are accessing us via ' +
+      'an insecure protocol (HTTP). ' +
+      'Redirecting you to HTTPS.'
+  );
+
+  window.location.href = window.location.href.replace('http:', 'https:');
+}
